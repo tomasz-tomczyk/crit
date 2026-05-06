1766f73a0cd206a52052eabb20cdda8d39e81ab7
Tomasz Tomczyk <me@tomasztomczyk.com>
2026-05-06 13:07:59 +0100

    feat(design): pre-ready agent command sender queue

---
+++ frontend/design-mode.queue.js
@@ -0,0 +1,27 @@
+'use strict';
+(function (root, factory) {
+  var api = factory();
+  if (typeof module === 'object' && module.exports) module.exports = api;
+  else {
+    root.crit = root.crit || {};
+    root.crit.design = root.crit.design || {};
+    root.crit.design.queue = api;
+  }
+})(typeof window !== 'undefined' ? window : globalThis, function () {
+  function makeAgentSender(opts) {
+    var post = opts.post;
+    var ready = false;
+    var queue = [];
+    function send(msg) {
+      if (!ready) { queue.push(msg); return; }
+      post(msg);
+    }
+    function requeue(msg) { queue.unshift(msg); }
+    function markReady() {
+      ready = true;
+      while (queue.length) post(queue.shift());
+    }
+    return { send: send, markReady: markReady, requeue: requeue };
+  }
+  return { makeAgentSender };
+});
